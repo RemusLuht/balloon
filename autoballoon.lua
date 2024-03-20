9a6bdf140922469dd1748c71bd58c649d9ed59de
RemusLuht <remusluht2005@gmail.com>
2024-03-20 11:30:30 +0200

    add

--- autoballoon.lua
+++ autoballoon.lua
@@ -1,144 +1,10 @@
 repeat task.wait(1) until game.PlaceId ~= nil
 repeat task.wait(1) until game:GetService("Players") and game:GetService("Players").LocalPlayer
 repeat task.wait(1) until not game.Players.LocalPlayer.PlayerGui:FindFirstChild("__INTRO")
-print("Loaded")
-getgenv().MoneyPrinter = {
-    toolName = "Slingshot",
-    autoBalloons = true,
-    autoPresents = true,
-    autoOpenGifts = true,
-
-    serverHopper = true,
-    avoidCooldown = true,
-    minServerTime = 10,
-
-    sendWeb = true,
-    webURL = "https://discord.com/api/webhooks/1197937174713217084/CE6-BwCF_cV1Qm5Wsosd6iyENIhv5yIVeQUX5eHlD6J8_cVEjvuCAw5w-Oe94a7y4HzC",
-
-    maybeCPUReducer = true,
-    debugMode = false,
-}
 if game:IsLoaded() and getgenv().MoneyPrinter.maybeCPUReducer then
-	print("Attempting CPU Reduce")
-	pcall(function()
-		for _, v in pairs(game:GetService("Workspace"):FindFirstChild("__THINGS"):GetChildren()) do
-			if table.find({"ShinyRelics", "Ornaments", "Instances", "Ski Chairs"}, v.Name) then
-				v:Destroy()
-			end
-		end
-
-		for _, v in pairs(game:GetService("Workspace"):FindFirstChild("__THINGS").__INSTANCE_CONTAINER.Active.AdvancedFishing:GetChildren()) do
-			if string.find(v.Name, "Model") or string.find(v.Name, "Water") or string.find(v.Name, "Debris") or string.find(v.Name, "Interactable") then
-				v:Destroy()
-			end
-
-			if v.Name == "Map" then
-				for _, v in pairs(v:GetChildren()) do
-					if v.Name ~= "Union" then
-						v:Destroy()
-					end
-				end
-			end
-		end
-
-		game:GetService("Workspace"):WaitForChild("ALWAYS_RENDERING"):Destroy()
-	end)
-
-	local Workspace = game:GetService("Workspace")
-	local Terrain = Workspace:WaitForChild("Terrain")
-	Terrain.WaterReflectance = 0
-	Terrain.WaterTransparency = 1
-	Terrain.WaterWaveSize = 0
-	Terrain.WaterWaveSpeed = 0
-
-	local Lighting = game:GetService("Lighting")
-	Lighting.Brightness = 0
-	Lighting.GlobalShadows = false
-	Lighting.FogEnd = 9e100
-	Lighting.FogStart = 0
-
-	sethiddenproperty(Lighting, "Technology", 2)
-	sethiddenproperty(Terrain, "Decoration", false)
-
-	local function clearTextures(v)
-		if v:IsA("BasePart") and not v:IsA("MeshPart") then
-			v.Material = "Plastic"
-			v.Reflectance = 0
-			v.Transparency = 1
-		elseif (v:IsA("Decal") or v:IsA("Texture")) then
-			v.Transparency = 1
-		elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-			v.Lifetime = NumberRange.new(0)
-		elseif v:IsA("Explosion") then
-			v.BlastPressure = 1
-			v.BlastRadius = 1
-		elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
-			v.Enabled = false
-		elseif v:IsA("MeshPart") then
-			v.Material = "Plastic"
-			v.Reflectance = 0
-			v.TextureID = 10385902758728957
-		elseif v:IsA("SpecialMesh")  then
-			v.TextureId = 0
-		elseif v:IsA("ShirtGraphic") then
-			v.Graphic = 1
-		elseif (v:IsA("Shirt") or v:IsA("Pants")) then
-			v[v.ClassName .. "Template"] = 1
-		elseif v.Name == "Foilage" and v:IsA("Folder") then
-			v:Destroy()
-		elseif string.find(v.Name, "Tree") or string.find(v.Name, "Water") or string.find(v.Name, "Bush") or string.find(v.Name, "grass") then
-			task.wait()
-			v:Destroy()
-		end
-	end
-
-	game:GetService("Lighting"):ClearAllChildren()
-
-	for _, v in pairs(Workspace:GetDescendants()) do
-		clearTextures(v)
-	end
-
-	Workspace.DescendantAdded:Connect(function(v)
-		clearTextures(v)
-	end)
-
-	for _, v in pairs(game.Players:GetChildren()) do
-		for _, v2 in pairs(v.Character:GetDescendants()) do
-			if v2:IsA("BasePart") or v2:IsA("Decal") then
-				v2.Transparency = 1
-			end
-		end
-	end
-
-	game.Players.PlayerAdded:Connect(function(player)
-		player.CharacterAdded:Connect(function(character)
-			for _, v in pairs(character:GetDescendants()) do
-				if v:IsA("BasePart") or v:IsA("Decal") then
-					v.Transparency = 1
-				end
-			end
-		end)
-	end)
-
-	for i,v in pairs(game.Players.LocalPlayer.PlayerGui:GetChildren()) do
-		if v:IsA("ScreenGui") then
-			v.Enabled = false
-		end
-	end
-
-	for i, v in pairs(game:GetService("StarterGui"):GetChildren()) do
-		if v:IsA("ScreenGui") then
-			v.Enabled = false
-		end
-	end
-
-	for i, v in pairs(game:GetService("CoreGui"):GetChildren()) do
-		if v:IsA("ScreenGui") then
-			v.Enabled = false
-		end
-	end
-	print("Completed CPU Reduce Attempt")
-end
+	loadstring(game:HttpGet("https://pastebin.com/raw/wiggpuqN"))()
+	setfpscap(8)
+end	
 local LargeRAP = 11000; local SmallRAP = 2800
 local RunService = game:GetService("RunService")
 local HttpService = game:GetService("HttpService")
@@ -226,6 +92,9 @@ autoLootBagConnection = workspace.__THINGS.Lootbags.ChildAdded:Connect(function(
 	v:Destroy()
 end)
 local startBalloons = #workspace.__THINGS.BalloonGifts:GetChildren()
+if #workspace.__THINGS.BalloonGifts:GetChildren() <= 1 then
+	repeat game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, getServer().id, Player) task.wait(3) until not game.PlaceId
+end
 local startGifts = 0
 local startLarge = 0
 for i,v in pairs(getInfo("Inventory").Misc) do
@@ -239,10 +108,10 @@ end
 local startTime = os.time()
 while getgenv().MoneyPrinter.autoBalloons do task.wait()
 	if getgenv().MoneyPrinter.autoPresents then getPresents() end
-	if not getTool() then equipTool(getgenv().MoneyPrinter.toolName) end
 	for _,Balloon in pairs(Library.Network.Invoke("BalloonGifts_GetActiveBalloons")) do task.wait(0.03)
 		if Balloon.Id then
 			while Library.Network.Invoke("BalloonGifts_GetActiveBalloons")[Balloon.Id] do task.wait(0.03)
+				if not getTool() then equipTool(getgenv().MoneyPrinter.toolName) end
 				local breakableId = getBalloonUID(getCurrentZone())
 				if breakableId == "Skip" then break end
 				if breakableId then
